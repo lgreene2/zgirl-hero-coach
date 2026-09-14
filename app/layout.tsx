1edb1c4b38ed8e3e6f63bebc1d5a862d76645af6
lgreene2 <lyndon.greene@csuglobal.edu>
2026-09-14 19:58:02 -0400

    Apply public version-label normalization across current app

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ import PWAClient from "@/components/PWAClient";
 import HomeMarketLanes from "@/components/HomeMarketLanes";
 import LegacyResourceLinks from "@/components/LegacyResourceLinks";
 import VoiceToTextEnhancer from "@/components/VoiceToTextEnhancer";
+import PublicVersionLabelNormalizer from "@/components/PublicVersionLabelNormalizer";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://zgirlinitiative.org"),
@@ -58,6 +59,7 @@ export default function RootLayout({
         <ConditionalPilotCTA />
         <LegacyResourceLinks />
         <VoiceToTextEnhancer />
+        <PublicVersionLabelNormalizer />
         <PWAClient />
         <Analytics />
       </body>
